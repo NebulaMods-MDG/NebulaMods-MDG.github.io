65a789d4fbee04906fa179616650b655a33df953
NebulaMods-MDG <grayson.schemenauer@gmail.com>
2026-07-01 12:08:19 -0500

    Update imagegen.js

--- imagegen.js
+++ imagegen.js
@@ -140,10 +140,9 @@ function generateImage()
 
             if (a < 10)
                 continue;
-
-const cm2X = y;
-const cm2Y = -x;
-const cm2Z = 0;
+const cm2X = 0;
+const cm2Y = width - 1 - x;
+const cm2Z = y;
 
 result.push(
     `14,0,${cm2X},${cm2Y},${cm2Z},${r}+${g}+${b}+1+0`
